0f78747ebea20df3bef733eaab7cea07a0609f64
Matai Cross <grantmatai@gmail.com>
2022-06-25 23:00:18 -0600

    commit

--- week6/scripts/chamber.js
+++ week6/scripts/chamber.js
@@ -31,7 +31,7 @@ fetch(apiURL)
 
     const iconsrc= `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
     const desc = jsObject.weather[0].description;
-    document.querySelector('#icon-src').textContent = iconsrc;
+    // document.querySelector('#icon-src').textContent = iconsrc;
     document.querySelector('#weathericon').setAttribute('src', iconsrc);
     document.querySelector('#weathericon').setAttribute('alt', desc);
     document.querySelector('figcaption').textContent = desc;
