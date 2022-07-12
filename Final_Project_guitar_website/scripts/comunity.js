3cd598c434fcfcb0d60715182ab3e31d3ffaf1a9
Matai Cross <grantmatai@gmail.com>
2022-07-12 16:32:15 -0600

    commit

--- Final_Project_guitar_website/scripts/comunity.js
+++ Final_Project_guitar_website/scripts/comunity.js
@@ -19,5 +19,27 @@ function goRight(){
 function goLeft(){
     document.querySelector(".carousel").scrollLeft -= 560;
 }
+function goRight2(){
+    document.querySelector(".carousel2").scrollLeft += 560;
+}
+
+function goLeft2(){
+    document.querySelector(".carousel2").scrollLeft -= 560;
+}
+function goRight3(){
+    document.querySelector(".carousel3").scrollLeft += 560;
+}
+
+function goLeft3(){
+    document.querySelector(".carousel3").scrollLeft -= 560;
+}
 // --------------
+
+function showVideos(group){
+     let genres = document.getElementById("genres");
+     for(let i = 0; i < genres.childElementCount; i++ ){
+         genres.children[i].classList.add("hidden");
+     }
+     document.getElementById(group).classList.remove("hidden");
+}
 
