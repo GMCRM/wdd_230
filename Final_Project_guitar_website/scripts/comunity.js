467f101e7001cffabea212d10e7b359deac7093b
Matai Cross <grantmatai@gmail.com>
2022-07-13 01:46:33 -0600

    commit

--- Final_Project_guitar_website/scripts/comunity.js
+++ Final_Project_guitar_website/scripts/comunity.js
@@ -43,16 +43,55 @@ function showVideos(group){
      document.getElementById(group).classList.remove("hidden");
 }
 
+window.addEventListener('load', () => {
+    let likeBtns = document.querySelectorAll('button.like-button');
+    for (let i = 0; i < likeBtns.length; i++) {
+        loadLike(likeBtns[i].getAttribute('data-postID'), likeBtns[i]);
+    }
+});
 
+function loadLike(postID, button) {
+    let likeKey = 'like-' + postID;
+    if(localStorage.getItem(likeKey)=="1") {
+        button.innerText = "Unlike";
+    } else {
+        button.innerText = "Like";
+    }
+}
 
-function like(button){
-    if(localStorage.getItem(button.name)=="1"){
+function like(postID, button){
+    let likeKey = 'like-' + postID;
+    if(localStorage.getItem(likeKey)=="1") {
         button.innerText="Like";
-        localStorage.setItem(button.name, "0");
-    
-    }
-    else{
+        localStorage.setItem(likeKey, "0");
+    }else{
         button.innerText="Unlike";
-        localStorage.setItem(button.name, "1");
+        localStorage.setItem(likeKey, "1");
     }
+}
+
+
+// local storage code
+let commentData = JSON.parse(localStorage.getItem("comments"));
+
+if (commentData !== null) {
+    for (let i = 0; i < commentData.length; i++) {
+        createComment(commentData[i].postID, commentData[i].value);
+    }
+} else {
+    commentData = [];
+}
+
+function postComment(postID) {
+    let value = document.getElementById('comment-box-' + postID).value;
+    commentData.push({postID: postID, value: value});
+    createComment(postID, value);
+    localStorage.setItem("comments", JSON.stringify(commentData));
+}
+
+function createComment(postID, value) {
+    let content = document.createElement('p'),
+        container = document.getElementById('comment-container-' + postID);
+    content.innerText = value;
+    container.appendChild(content);
 }
